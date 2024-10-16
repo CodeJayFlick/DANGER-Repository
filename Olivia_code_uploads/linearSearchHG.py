4a490c0c82b5c2e433688c172274356e1f9793d9
olivia11011 <oliviarshafer@gmail.com>
2024-10-16 15:55:40 -0400

    Added 2 Human Generated Code

---
+++ Olivia_code_uploads/linearSearchHG.py
@@ -0,0 +1,11 @@
+# linear search
+
+def linear(srchlist, srch):
+	"""check every element, kinda slow"""
+	
+	for element in srchlist:
+		if element == srch:
+			return srchlist.index(element)
+		
+	return -1
+	
